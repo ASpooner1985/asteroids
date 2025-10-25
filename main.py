86fede27cbb2d7d21768e24c0fd9d5d17b8befc8
ASpooner1985 <florian@florian-focht.de>
2025-10-25 12:31:01 +0000

    Erster Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("green")
+        pygame.display.flip()
+        print("Hallo")
+        
+
+
+if __name__ == "__main__":
+    main()
+
+
