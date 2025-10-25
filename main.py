abd41d88e705f784efe52ec15fd35b4bef7d890e
ASpooner1985 <florian@florian-focht.de>
2025-10-25 12:49:26 +0000

    Erste Spielaktion

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    newPlayer = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("green")
+        newPlayer.draw(screen)
         pygame.display.flip()
 
         returnTime = clock.tick(60)
